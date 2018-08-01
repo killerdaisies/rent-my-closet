050b69ac91dcc2fd3e606fbc74b130ef9d026bbc
dylan wang <zywang_dylan@126.com>
2018-08-01 17:45:58 +0800

    app.js bug fix

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //app.js
-
 App({
   onLaunch: function () {
     const host = 'https://rent-my-closet.herokuapp.com/'
@@ -8,7 +7,7 @@ App({
     wx.login({
       success: (res) => {
         console.log(res)
-        let currentUser = wx.getStorageSync('currentUser') 
+        let currentUser = wx.getStorageSync('currentUser')
         if (!currentUser) {
           wx.request({
             url: host + 'api/v1/login',
@@ -23,6 +22,11 @@ App({
               console.log(11, res1)
               // wx.getUserInfo({
               //   success: function (res2) {
+            }
+          })
+        }
+      }
+    })
   },
   globalData: {}
 })
