0ad905970e2153cf5534f9b9c8d017f50f87cb66
dylan wang <zywang_dylan@126.com>
2018-08-01 15:51:07 +0800

    change the addpage css

--- app.js
+++ app.js
@@ -2,75 +2,75 @@
 
 App({
   onLaunch: function () {
-    // const host = 'https://rent-my-closet.herokuapp.com/'
-    const host = "http://localhost:3000/"
-    // const host = 'https://localhost.3000(what is your address)/'
-    wx.login({
-      success: (res) => {
-        console.log(res)
-        let currentUser = wx.getStorageSync('currentUser') 
-        if (!currentUser) {
-          wx.request({
-            url: host + 'api/v1/login',
-            method: 'post',
-            data: {
-              code: res.code
-            },
-            success: (res1) => {
-              console.log(1, res1)
-              // this.globalData.userId = res1.data.userId
-              const page = this
-              console.log(11, res1)
-              // wx.getUserInfo({
-              //   success: function (res2) {
+  //   // const host = 'https://rent-my-closet.herokuapp.com/'
+  //   const host = "http://localhost:3000/"
+  //   // const host = 'https://localhost.3000(what is your address)/'
+  //   wx.login({
+  //     success: (res) => {
+  //       console.log(res)
+  //       let currentUser = wx.getStorageSync('currentUser') 
+  //       if (!currentUser) {
+  //         wx.request({
+  //           url: host + 'api/v1/login',
+  //           method: 'post',
+  //           data: {
+  //             code: res.code
+  //           },
+  //           success: (res1) => {
+  //             console.log(1, res1)
+  //             // this.globalData.userId = res1.data.userId
+  //             const page = this
+  //             console.log(11, res1)
+  //             // wx.getUserInfo({
+  //             //   success: function (res2) {
 
-              //     page.globalData.userInfo = res2.userInfo
-              //     wx.request({
-              //       url: `http://localhost:3000/api/v1/users/${res1.data.userId}`,
-              //       method: 'put',
-              //       data: {
-              //         name: res2.userInfo.nickName,
-              //         gender: res2.userInfo.gender,
-              //         avatarUrl: res2.userInfo.avatarUrl
-              //       },
-              //       success: function (res) {
-              //         // set data on index page and show
-              //         // wx.redirectTo({
-              //         //   url: '/pages/main/main'
-              //         // });
-              //         wx.setStorageSync('currentUser', res.data)
-              //       }
-              //       // insert next code here
-              //     })
-              //   },
-              // })
+  //             //     page.globalData.userInfo = res2.userInfo
+  //             //     wx.request({
+  //             //       url: `http://localhost:3000/api/v1/users/${res1.data.userId}`,
+  //             //       method: 'put',
+  //             //       data: {
+  //             //         name: res2.userInfo.nickName,
+  //             //         gender: res2.userInfo.gender,
+  //             //         avatarUrl: res2.userInfo.avatarUrl
+  //             //       },
+  //             //       success: function (res) {
+  //             //         // set data on index page and show
+  //             //         // wx.redirectTo({
+  //             //         //   url: '/pages/main/main'
+  //             //         // });
+  //             //         wx.setStorageSync('currentUser', res.data)
+  //             //       }
+  //             //       // insert next code here
+  //             //     })
+  //             //   },
+  //             // })
 
-            }
-          })
-        }
-      }
-    })
-  }
-    // const host = 'http://localhost:3000/'
-    // console.log('processing to login')
-    // wx.login({
-    //   success: (res) => {
-    //     console.log(res)
-    //     // insert next code here
-    //     wx.request({
-    //       url: host + 'api/v1/login',
-    //       method: 'POST',
-    //       data: {
-    //         code: res.code
-    //       },
-    //       // insert next code here
-    //       success: (res) => {
-    //         console.log(res)
-    //         this.globalData.userId = res.data.userId
-    //       }
-    //     })
-    //   }
-    // })
+  //           }
+  //         })
+  //       }
+  //     }
+  //   })
+  // }
+  //   // const host = 'http://localhost:3000/'
+  //   // console.log('processing to login')
+  //   // wx.login({
+  //   //   success: (res) => {
+  //   //     console.log(res)
+  //   //     // insert next code here
+  //   //     wx.request({
+  //   //       url: host + 'api/v1/login',
+  //   //       method: 'POST',
+  //   //       data: {
+  //   //         code: res.code
+  //   //       },
+  //   //       // insert next code here
+  //   //       success: (res) => {
+  //   //         console.log(res)
+  //   //         this.globalData.userId = res.data.userId
+  //   //       }
+  //   //     })
+  //   //   }
+  //   // })
   },
   globalData: {}
 })
